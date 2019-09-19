003ff408d6ca9309ddef93dded6e41f916d8f196
Nitish Aggarwal <royalnitish21@gmail.com>
2019-09-19 14:41:30 +0530

    feat: Add refer and earn with credify limit basic UI

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -52,12 +52,87 @@ class _DashboardScreenState extends State<DashboardScreen> {
                   ],
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(36, 8, 36, 8),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text("Used",
+                              style:
+                                  Theme.of(context).accentTextTheme.display4),
+                          Text("Credify limit",
+                              style: Theme.of(context).accentTextTheme.display4)
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text("4,000/",
+                              style: Theme.of(context)
+                                  .accentTextTheme
+                                  .display2
+                                  .copyWith(fontWeight: FontWeight.bold)),
+                          Text("10,000/",
+                              style: Theme.of(context)
+                                  .accentTextTheme
+                                  .display2
+                                  .copyWith(fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(36, 8, 36, 16),
+                child: Container(
+                  height: 130,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Color.fromRGBO(47, 128, 237, .8)),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
+                          child: Text(
+                              "Add friends and increase your score"
+                                  .toUpperCase(),
+                              softWrap: true,
+                              style: Theme.of(context)
+                                  .primaryTextTheme
+                                  .display3
+                                  .copyWith(fontSize: 19, letterSpacing: 3)),
+                        ),
+                        width: MediaQuery.of(context).size.width / 2,
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Container(
+                            child: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                radius: 30,
+                                child: Image.asset(
+                                  "assets/images/addFriend.png",
+                                  height: 40,
+                                  width: 40,
+                                )),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
               GestureDetector(
-                onTap: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => TravelScreen()));
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TravelScreen()));
                 },
                 child: Padding(
                   padding: const EdgeInsets.only(top: 10, bottom: 10),
@@ -72,7 +147,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                padding: const EdgeInsets.only(top: 10, bottom: 30),
                 child: DashboardCard(
                   isAtBottom: true,
                   heading: "Your Ride. Your Bike",
