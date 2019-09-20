79270cca0366d6fe9d0d36694a60d96d6096fa8f
Nitish Aggarwal <royalnitish21@gmail.com>
2019-09-20 10:21:05 +0530

    feat: Add BankScreen1

---
+++ lib/bank_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class BankScreen extends StatefulWidget {
+  @override
+  _BankScreenState createState() => _BankScreenState();
+}
+
+class _BankScreenState extends State<BankScreen> {
+  String bankName = "HDFC Bank";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(3, 4, 5, 1),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 50, left: 30),
+                child: Text(
+                  "Credify",
+                  style: Theme.of(context).primaryTextTheme.display2,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20, left: 30),
+                child: Text(
+                  " Bank Statement",
+                  style: Theme.of(context).primaryTextTheme.display1,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 0, left: 30),
+                child: Text(
+                  "  1/2",
+                  style: Theme.of(context).primaryTextTheme.display3,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 60, left: 30),
+                child: Text(
+                  " Select your bank",
+                  style: Theme.of(context).primaryTextTheme.display2,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40, 30, 40, 5),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: bankName,
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: Theme.of(context).primaryTextTheme.display3,
+                      onChanged: (String newValue) {
+                        setState(() {
+                          bankName = newValue;
+                        });
+                      },
+                      items: <String>['HDFC Bank']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: Theme.of(context).primaryTextTheme.display3,
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
