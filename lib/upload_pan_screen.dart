04b1ffd94c6db2fc4c9ed60956d22d3c22632895
Nitish Aggarwal <royalnitish21@gmail.com>
2019-09-28 18:14:58 +0530

    chor: Add ProgressIndicators in Aadhar and Pan upload screens..

--- lib/upload_pan_screen.dart
+++ lib/upload_pan_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:archive/archive_io.dart';
 import 'package:credify/globals.dart';
 import 'package:credify/services/add_documents.dart';
+import 'package:credify/undismissable_progress_bar.dart';
 import 'package:credify/video_selfie_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -40,205 +41,215 @@ class _UploadPanState extends State<UploadPan> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(3, 4, 5, 1),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50, left: 30),
-                child: Text(
-                  "Credify",
-                  style: Theme.of(context).primaryTextTheme.display2,
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20, left: 30),
-                child: Text(
-                  " Upload",
-                  style: Theme.of(context).primaryTextTheme.display1,
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 0, left: 30),
-                child: Text(
-                  "  2/2",
-                  style: Theme.of(context).primaryTextTheme.display3,
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 10, left: 30),
-                child: Text(
-                  "  PAN Card",
-                  style: Theme.of(context).primaryTextTheme.display4,
-                ),
-              ),
-            ),
-            Column(
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Column(
               children: <Widget>[
                 Align(
-                  alignment: Alignment.center,
+                  alignment: Alignment.topLeft,
                   child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 20, left: 20, bottom: 20),
+                    padding: const EdgeInsets.only(top: 50, left: 30),
                     child: Text(
-                      " Upload PAN card Front photo",
-                      style: Theme.of(context).primaryTextTheme.display3,
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Container(
-                      child: GestureDetector(
-                          onTap: () {
-                            getFrontImage();
-                          },
-                          child: _frontPanImage == null
-                              ? Container(
-                                  decoration: BoxDecoration(
-                                      border: Border.all(color: Colors.white)),
-                                  child: Padding(
-                                      padding: EdgeInsets.all(10),
-                                      child: Image.asset(
-                                        "assets/images/camera.png",
-                                        height: 50,
-                                        width: 50,
-                                      )))
-                              : Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: Container(
-                                    height: 120,
-                                    width: 200,
-                                    child: Image.file(
-                                      _frontPanImage,
-                                      fit: BoxFit.fitWidth,
-                                    ),
-                                  ),
-                                )),
+                      "Credify",
+                      style: Theme.of(context).primaryTextTheme.display2,
                     ),
                   ),
                 ),
                 Align(
-                  alignment: Alignment.center,
+                  alignment: Alignment.topLeft,
                   child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 20, left: 20, bottom: 20),
+                    padding: const EdgeInsets.only(top: 20, left: 30),
                     child: Text(
-                      " Upload PAN card Back photo",
+                      " Upload",
+                      style: Theme.of(context).primaryTextTheme.display1,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 0, left: 30),
+                    child: Text(
+                      "  2/2",
                       style: Theme.of(context).primaryTextTheme.display3,
                     ),
                   ),
                 ),
-                Center(
+                Align(
+                  alignment: Alignment.topLeft,
                   child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Container(
-                      child: GestureDetector(
-                          onTap: () {
-                            getBackImage();
-                          },
-                          child: _backPanImage == null
-                              ? Container(
-                                  decoration: BoxDecoration(
-                                      border: Border.all(color: Colors.white)),
-                                  child: Padding(
-                                      padding: EdgeInsets.all(10),
-                                      child: Image.asset(
-                                        "assets/images/camera.png",
-                                        height: 50,
-                                        width: 50,
-                                      )))
-                              : Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: Container(
-                                    height: 120,
-                                    width: 200,
-                                    child: Image.file(
-                                      _backPanImage,
-                                      fit: BoxFit.fitWidth,
-                                    ),
-                                  ),
-                                )),
+                    padding: const EdgeInsets.only(top: 10, left: 30),
+                    child: Text(
+                      "  PAN Card",
+                      style: Theme.of(context).primaryTextTheme.display4,
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 30.0),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: RaisedButton(
-                      elevation: 5.0,
-                      color: Color.fromRGBO(45, 156, 219, 1),
-                      shape: new RoundedRectangleBorder(
-                          side: BorderSide(
-                            style: BorderStyle.solid,
-                            width: 2,
-                          ),
-                          borderRadius: new BorderRadius.circular(5)),
+                Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.center,
                       child: Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(60.0, 20.0, 60.0, 20.0),
-                        child: new Text('Continue',
-                            style: new TextStyle(
-                                fontSize: 18.0, color: Colors.white)),
+                        padding: const EdgeInsets.only(
+                            top: 20, left: 20, bottom: 20),
+                        child: Text(
+                          " Upload PAN card Front photo",
+                          style: Theme.of(context).primaryTextTheme.display3,
+                        ),
                       ),
-                      onPressed: () async {
-                        if (_frontPanImage != null && _backPanImage != null) {
-                          setState(() {
-                            isLoading = true;
-                          });
-                          Directory tempDir =
-                              await getApplicationDocumentsDirectory();
-                          String tempPath = tempDir.path;
-                          var encoder = ZipFileEncoder();
-                          encoder.create(tempPath + "/pan.zip");
-                          encoder.addFile(_frontPanImage);
-                          encoder.addFile(_backPanImage);
-                          encoder.close();
-                          bool isUploaded = await addDocumentsService(
-                              currentUserId, "pan", 1, encoder);
-                          print(isUploaded);
-                          if (isUploaded) {
-                            setState(() {
-                              isLoading = false;
-                            });
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => VideoSelfieScreen()));
-                          } else {
-                            Toast.show(
-                              "Something Wrong Occured",
-                              context,
-                              duration: Toast.LENGTH_LONG,
-                            );
-                          }
-                        } else {
-                          Toast.show(
-                            "You have not choosen one or more images",
-                            context,
-                            duration: Toast.LENGTH_LONG,
-                          );
-                        }
-                      },
                     ),
-                  ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Container(
+                          child: GestureDetector(
+                              onTap: () {
+                                getFrontImage();
+                              },
+                              child: _frontPanImage == null
+                                  ? Container(
+                                      decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.white)),
+                                      child: Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Image.asset(
+                                            "assets/images/camera.png",
+                                            height: 50,
+                                            width: 50,
+                                          )))
+                                  : Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Container(
+                                        height: 120,
+                                        width: 200,
+                                        child: Image.file(
+                                          _frontPanImage,
+                                          fit: BoxFit.fitWidth,
+                                        ),
+                                      ),
+                                    )),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20, left: 20, bottom: 20),
+                        child: Text(
+                          " Upload PAN card Back photo",
+                          style: Theme.of(context).primaryTextTheme.display3,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Container(
+                          child: GestureDetector(
+                              onTap: () {
+                                getBackImage();
+                              },
+                              child: _backPanImage == null
+                                  ? Container(
+                                      decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.white)),
+                                      child: Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Image.asset(
+                                            "assets/images/camera.png",
+                                            height: 50,
+                                            width: 50,
+                                          )))
+                                  : Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Container(
+                                        height: 120,
+                                        width: 200,
+                                        child: Image.file(
+                                          _backPanImage,
+                                          fit: BoxFit.fitWidth,
+                                        ),
+                                      ),
+                                    )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 30.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: RaisedButton(
+                          elevation: 5.0,
+                          color: Color.fromRGBO(45, 156, 219, 1),
+                          shape: new RoundedRectangleBorder(
+                              side: BorderSide(
+                                style: BorderStyle.solid,
+                                width: 2,
+                              ),
+                              borderRadius: new BorderRadius.circular(5)),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(
+                                60.0, 20.0, 60.0, 20.0),
+                            child: new Text('Continue',
+                                style: new TextStyle(
+                                    fontSize: 18.0, color: Colors.white)),
+                          ),
+                          onPressed: () async {
+                            if (_frontPanImage != null &&
+                                _backPanImage != null) {
+                              setState(() {
+                                isLoading = true;
+                              });
+                              Directory tempDir =
+                                  await getApplicationDocumentsDirectory();
+                              String tempPath = tempDir.path;
+                              var encoder = ZipFileEncoder();
+                              encoder.create(tempPath + "/pan.zip");
+                              encoder.addFile(_frontPanImage);
+                              encoder.addFile(_backPanImage);
+                              encoder.close();
+                              bool isUploaded = await addDocumentsService(
+                                  currentUserId, "pan", 1, encoder);
+                              print(isUploaded);
+                              if (isUploaded) {
+                                setState(() {
+                                  isLoading = false;
+                                });
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            VideoSelfieScreen()));
+                              } else {
+                                Toast.show(
+                                  "Something Wrong Occured",
+                                  context,
+                                  duration: Toast.LENGTH_LONG,
+                                );
+                              }
+                            } else {
+                              Toast.show(
+                                "You have not choosen one or more images",
+                                context,
+                                duration: Toast.LENGTH_LONG,
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+          UndismissableProgressBar()
+        ],
       ),
     );
   }
