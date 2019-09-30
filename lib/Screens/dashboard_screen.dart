88b21a9e9eb32edaa1984092c8e9bd9163a4b7d7
Nitish Aggarwal <royalnitish21@gmail.com>
2019-09-30 21:57:54 +0530

    feat: Change progress bar according to kyc status

--- lib/Screens/dashboard_screen.dart
+++ lib/Screens/dashboard_screen.dart
@@ -173,7 +173,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(top: 100, bottom: 40),
+                          padding: const EdgeInsets.only(top: 100, bottom: 20),
                           child: CredifyCard(
                             cardLevel: "Silver",
                             cardNumber: currentUserCardNumber,
@@ -188,64 +188,127 @@ class _DashboardScreenState extends State<DashboardScreen> {
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(36, 8, 36, 8),
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text("Used",
-                                  style: Theme.of(context)
-                                      .accentTextTheme
-                                      .display4),
-                              Text("Credify limit",
-                                  style: Theme.of(context)
-                                      .accentTextTheme
-                                      .display4)
-                            ],
+                  currentUserData == null
+                      ? Padding(
+                          padding: const EdgeInsets.fromLTRB(36, 8, 36, 8),
+                          child: Container(
+                            height: 90,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text("4,000/",
-                                  style: Theme.of(context)
-                                      .accentTextTheme
-                                      .display2
-                                      .copyWith(fontWeight: FontWeight.bold)),
-                              Text("10,000/",
-                                  style: Theme.of(context)
-                                      .accentTextTheme
-                                      .display2
-                                      .copyWith(fontWeight: FontWeight.bold))
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
+                        )
+                      : (currentUserData.kycProgress == 3)
+                          ? Padding(
+                              padding: const EdgeInsets.fromLTRB(36, 8, 36, 8),
+                              child: Container(
+                                height: 90,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text("Used",
+                                            style: Theme.of(context)
+                                                .accentTextTheme
+                                                .display4),
+                                        Text("Credify limit",
+                                            style: Theme.of(context)
+                                                .accentTextTheme
+                                                .display4)
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text("4,000/",
+                                            style: Theme.of(context)
+                                                .accentTextTheme
+                                                .display2
+                                                .copyWith(
+                                                    fontWeight:
+                                                        FontWeight.bold)),
+                                        Text("10,000/",
+                                            style: Theme.of(context)
+                                                .accentTextTheme
+                                                .display2
+                                                .copyWith(
+                                                    fontWeight:
+                                                        FontWeight.bold))
+                                      ],
+                                    ),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                                      child: Card(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                        ),
+                                        elevation: 10.0,
+                                        child: ProgressBar(
+                                          linearGradient: LinearGradient(
+                                              begin: Alignment.centerLeft,
+                                              end: Alignment.centerRight,
+                                              colors: [
+                                                Color.fromRGBO(229, 93, 135, 1),
+                                                Color.fromRGBO(95, 195, 228, 1)
+                                              ]),
+                                          progress: 100.0,
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          height: 15.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              elevation: 10.0,
-                              child: ProgressBar(
-                                linearGradient: LinearGradient(
-                                    begin: Alignment.centerLeft,
-                                    end: Alignment.centerRight,
-                                    colors: [
-                                      Color.fromRGBO(229, 93, 135, 1),
-                                      Color.fromRGBO(95, 195, 228, 1)
-                                    ]),
-                                progress: 40.0,
-                                borderRadius: BorderRadius.circular(10.0),
-                                height: 15.0,
+                            )
+                          : Padding(
+                              padding: const EdgeInsets.fromLTRB(36, 8, 36, 8),
+                              child: Container(
+                                height: 90,
+                                child: Column(
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: <Widget>[
+                                        Text("Credify limit",
+                                            style: Theme.of(context)
+                                                .accentTextTheme
+                                                .display4)
+                                      ],
+                                    ),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                                      child: Card(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                        ),
+                                        elevation: 10.0,
+                                        child: ProgressBar(
+                                          linearGradient: LinearGradient(
+                                              begin: Alignment.centerLeft,
+                                              end: Alignment.centerRight,
+                                              colors: [
+                                                Color.fromRGBO(
+                                                    221, 221, 221, 1),
+                                                Color.fromRGBO(221, 221, 221, 1)
+                                              ]),
+                                          progress: 40.0,
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          height: 15.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
                   GestureDetector(
                     onTap: () async {
                       List<Contact> listOfContacts = await getContacts();
