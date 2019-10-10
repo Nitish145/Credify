cea5f0bad948bae552a5e554631c64c9c278d355
Nitish Aggarwal <royalnitish21@gmail.com>
2019-10-11 01:27:56 +0530

    chor: UI enhancement in referral screen

--- lib/Screens/referral_screen.dart
+++ lib/Screens/referral_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ReferralScreen extends StatefulWidget {
   @override
@@ -7,11 +9,83 @@ class ReferralScreen extends StatefulWidget {
 }
 
 class _ReferralScreenState extends State<ReferralScreen> {
+  String referralLink = "http://www.statholdings.com";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 100),
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 150),
+                    child: Container(
+                      child: Center(
+                        child: SvgPicture.asset(
+                          "assets/svg/refer.svg",
+                          semanticsLabel: "Refer Background",
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 24, 0, 4),
+                    child: Text(
+                      "Here's your refer link",
+                      style: Theme.of(context).accentTextTheme.display3,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                    child: Container(
+                      color: Color.fromRGBO(232, 232, 232, 1),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Text(
+                              referralLink,
+                              style: Theme.of(context).accentTextTheme.display4,
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Clipboard.setData(
+                                  new ClipboardData(text: referralLink));
+                              Fluttertoast.showToast(
+                                  msg: "Link copied to clipboard");
+                            },
+                            child: Container(
+                              color: Colors.grey.withOpacity(0.5),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.asset(
+                                  "assets/images/clipboardIcon.png",
+                                  height: 25,
+                                  width: 25,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 24, 0, 40),
+                    child: Text(
+                      "Share the link with your friends",
+                      style: Theme.of(context).accentTextTheme.display4,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           Container(
             width: MediaQuery.of(context).size.width,
             child: SafeArea(
@@ -41,69 +115,6 @@ class _ReferralScreenState extends State<ReferralScreen> {
               ),
             ),
           ),
-          SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 250),
-                  child: Container(
-                    child: Center(
-                      child: SvgPicture.asset(
-                        "assets/svg/refer.svg",
-                        semanticsLabel: "Refer Background",
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 24, 0, 4),
-                  child: Text(
-                    "Here's your refer link",
-                    style: Theme.of(context).accentTextTheme.display3,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                  child: Container(
-                    color: Color.fromRGBO(232, 232, 232, 1),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Text(
-                            "http://www.statholdings.com",
-                            style: Theme.of(context).accentTextTheme.display4,
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {},
-                          child: Container(
-                            color: Colors.grey.withOpacity(0.5),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Image.asset(
-                                "assets/images/clipboardIcon.png",
-                                height: 25,
-                                width: 25,
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 24, 0, 4),
-                  child: Text(
-                    "Share the link with your friends",
-                    style: Theme.of(context).accentTextTheme.display4,
-                  ),
-                ),
-              ],
-            ),
-          )
         ],
       ),
     );
