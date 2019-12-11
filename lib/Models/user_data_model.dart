4058f65dff537f88e18da50f6d43889da39eff00
Nitish Aggarwal <royalnitish21@gmail.com>
2019-12-11 13:00:14 +0530

    bfix: Updated USerData model to parse response properly

--- lib/Models/user_data_model.dart
+++ lib/Models/user_data_model.dart
@@ -10,64 +10,80 @@ String userDataToJson(UserData data) => json.encode(data.toJson());
 
 class UserData {
   String id;
-  String mobileNumber;
-  bool kycStatus;
-  int kycProgress;
-  String kycId;
   bool bankDetailsProvided;
-  bool bankingVerified;
   String bankingId;
+  bool bankingVerified;
+  String cardNumber;
+  String documentsId;
   int documentsProgress;
   bool documentsUploaded;
-  String documentsId;
-  List<String> packages;
-  List<String> groupId;
+  List<dynamic> groupId;
+  String kycId;
+  int kycProgress;
+  bool kycStatus;
+  String mobileNumber;
+  List<dynamic> packages;
+  int referrBonus;
+  String referralCode;
+  String referredBy;
 
   UserData({
     this.id,
-    this.mobileNumber,
-    this.kycStatus,
-    this.kycProgress,
-    this.kycId,
     this.bankDetailsProvided,
-    this.bankingVerified,
     this.bankingId,
+    this.bankingVerified,
+    this.cardNumber,
+    this.documentsId,
     this.documentsProgress,
     this.documentsUploaded,
-    this.documentsId,
-    this.packages,
     this.groupId,
+    this.kycId,
+    this.kycProgress,
+    this.kycStatus,
+    this.mobileNumber,
+    this.packages,
+    this.referrBonus,
+    this.referralCode,
+    this.referredBy,
   });
 
   factory UserData.fromJson(Map<String, dynamic> json) => UserData(
-        id: json["_id"],
-        mobileNumber: json["mobile_number"],
-        kycStatus: json["kyc_status"],
-        kycProgress: json["kyc_progress"],
-        kycId: json["kyc_id"],
-        bankDetailsProvided: json["bank_details_provided"],
-        bankingVerified: json["banking_verified"],
-        bankingId: json["banking_id"],
-        documentsProgress: json["documents_progress"],
-        documentsUploaded: json["documents_uploaded"],
-        documentsId: json["documents_id"],
-        packages: List<String>.from(json["packages"].map((x) => x)),
-        groupId: List<String>.from(json["group_id"].map((x) => x)),
-      );
+    id: json["_id"],
+    bankDetailsProvided: json["bank_details_provided"],
+    bankingId: json["banking_id"],
+    bankingVerified: json["banking_verified"],
+    cardNumber: json["card_number"],
+    documentsId: json["documents_id"],
+    documentsProgress: json["documents_progress"],
+    documentsUploaded: json["documents_uploaded"],
+    groupId: List<dynamic>.from(json["group_id"].map((x) => x)),
+    kycId: json["kyc_id"],
+    kycProgress: json["kyc_progress"],
+    kycStatus: json["kyc_status"],
+    mobileNumber: json["mobile_number"],
+    packages: List<dynamic>.from(json["packages"].map((x) => x)),
+    referrBonus: json["referr_bonus"],
+    referralCode: json["referral_code"],
+    referredBy: json["referred_by"],
+  );
 
   Map<String, dynamic> toJson() => {
-        "_id": id,
-        "mobile_number": mobileNumber,
-        "kyc_status": kycStatus,
-        "kyc_progress": kycProgress,
-        "kyc_id": kycId,
-        "bank_details_provided": bankDetailsProvided,
-        "banking_verified": bankingVerified,
-        "banking_id": bankingId,
-        "documents_progress": documentsProgress,
-        "documents_uploaded": documentsUploaded,
-        "documents_id": documentsId,
-        "packages": List<dynamic>.from(packages.map((x) => x)),
-        "group_id": List<dynamic>.from(groupId.map((x) => x)),
-      };
+    "_id": id,
+    "bank_details_provided": bankDetailsProvided,
+    "banking_id": bankingId,
+    "banking_verified": bankingVerified,
+    "card_number": cardNumber,
+    "documents_id": documentsId,
+    "documents_progress": documentsProgress,
+    "documents_uploaded": documentsUploaded,
+    "group_id": List<dynamic>.from(groupId.map((x) => x)),
+    "kyc_id": kycId,
+    "kyc_progress": kycProgress,
+    "kyc_status": kycStatus,
+    "mobile_number": mobileNumber,
+    "packages": List<dynamic>.from(packages.map((x) => x)),
+    "referr_bonus": referrBonus,
+    "referral_code": referralCode,
+    "referred_by": referredBy,
+  };
 }
